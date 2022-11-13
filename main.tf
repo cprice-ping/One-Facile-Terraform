94b8e8c32ba05d39637fe4c78e1ee55a38d582d4
Chris Price <cprice@pingidentity.com>
2022-11-13 08:45:34 -0800

    Testing license lookup

--- main.tf
+++ main.tf
@@ -29,70 +29,40 @@ data "pingone_licenses" "internal_license" {
   }
 }
 
-resource "pingone_environment" "release_environment" {
-  name        = var.env_name
-  description = "Created by Terraform"
-  type        = "PRODUCTION"
-  license_id  = data.pingone_licenses.internal_license.id
+# resource "pingone_environment" "release_environment" {
+#   name        = var.env_name
+#   description = "Created by Terraform"
+#   type        = "PRODUCTION"
+#   license_id  = data.pingone_licenses.internal_license.id
 
-  default_population {}
-  service {
-    type = "SSO"
-  }
-  service {
-    type = "MFA"
-  }
-  service {
-    type = "Risk"
-  }
-  # service {
-  #   type = "Authorize"
-  # }
-  service {
-    type = "DaVinci"
-  }
-}
-
-resource "pingone_application" "oidc_login_app" {
-  environment_id = pingone_environment.release_environment.id
-  name           = "OIDC Login"
-  enabled        = true
-
-  oidc_options {
-    type                        = "WEB_APP"
-    grant_types                 = ["AUTHORIZATION_CODE", "REFRESH_TOKEN"]
-    response_types              = ["CODE"]
-    token_endpoint_authn_method = "CLIENT_SECRET_BASIC"
-    redirect_uris               = ["https://decoder.pingidentity.cloud/oidc", "https://decoder.pingidentity.cloud/hybrid"]
-  }
-}
-
-# resource "pingone_custom_domain" "custom_domain" {
-#   environment_id = pingone_environment.release_environment.id
-
-#   domain_name = "login.${var.dns_domain_name}"
+#   default_population {}
+#   service {
+#     type = "SSO"
+#   }
+#   service {
+#     type = "MFA"
+#   }
+#   service {
+#     type = "Risk"
+#   }
+#   # service {
+#   #   type = "Authorize"
+#   # }
+#   service {
+#     type = "DaVinci"
+#   }
 # }
 
-# resource "pingone_custom_domain_verify" "custom_domain" {
+# resource "pingone_application" "oidc_login_app" {
 #   environment_id = pingone_environment.release_environment.id
+#   name           = "OIDC Login"
+#   enabled        = true
 
-#   custom_domain_id = pingone_custom_domain.custom_domain.id
-
-#   depends_on = [
-#     dnsimple_zone_record.prettierbaby_login
-#   ]
-# }
-
-# resource "pingone_custom_domain_ssl" "custom_domain" {
-#   environment_id = pingone_environment.release_environment.id
-
-#   custom_domain_id = pingone_custom_domain.custom_domain.id
-
-#   certificate_pem_file               = acme_certificate.certificate.certificate_pem
-#   intermediate_certificates_pem_file = acme_certificate.certificate.issuer_pem
-#   private_key_pem_file               = acme_certificate.certificate.private_key_pem
-
-#   depends_on = [
-#     pingone_custom_domain_verify.custom_domain
-#   ]
+#   oidc_options {
+#     type                        = "WEB_APP"
+#     grant_types                 = ["AUTHORIZATION_CODE", "REFRESH_TOKEN"]
+#     response_types              = ["CODE"]
+#     token_endpoint_authn_method = "CLIENT_SECRET_BASIC"
+#     redirect_uris               = ["https://decoder.pingidentity.cloud/oidc", "https://decoder.pingidentity.cloud/hybrid"]
+#   }
 # }
