df21db96340c5b71f564a446e592fbab9aea3996
Chris Price <cprice@pingidentity.com>
2022-11-14 15:51:57 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "pingone_resource" {
-    value = data.pingone_resource.pingone_resource.name
+output "release_env_id" {
+    value = pingone_environment.release_environment.id
+}
+
+output "oidc_client_id" {
+    value = pingone_application.oidc_login_app.id
 }
